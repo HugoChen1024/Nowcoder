cba265ce01f005a5c4f1879c6bd15637cc2ba633
ranjiewwen <532685734@qq.com>
2016-03-10 18:20:37 +0800

    华为上机测试

---
+++ 20160310_01.cpp
@@ -0,0 +1,87 @@
+﻿//题目描述
+//
+//•连续输入字符串，请按长度为8拆分每个字符串后输出到新的字符串数组；
+//•长度不是8整数倍的字符串请在后面补数字0，空字符串不处理。
+//
+//输入描述 :
+//连续输入字符串(输入2次, 每个字符串长度小于100)
+//
+//
+//输出描述 :
+//	 输出到长度为8的新字符串数组
+
+#include<iostream>
+#include<string>
+using namespace std;
+void fun(string str,int len)
+{
+	int m = len % 8;//余数
+	int n = len / 8;//整数；
+	int i = 0;
+	while (i<n)
+	{
+		for (size_t k = 0; k < 8; k++)
+		{
+			cout << str[i * 8 + k];
+		}
+		i++;
+		cout << endl;
+	}
+	if (m!=0)
+	{
+		for (size_t k = 0; k < m; k++)
+		{
+			cout << str[n * 8 + k];
+		}
+		for (size_t k = m; k < 8; k++)
+		{
+			cout << 0;
+		}
+		cout << endl;
+	}
+}
+int main()
+{
+	string str1, str2;
+	cin >> str1 >> str2;
+	int len1 = str1.length();
+	int len2 = str2.size();
+	fun(str1,len1);
+	fun(str2, len2);
+	return 0;
+
+	/*string str,str1;
+	cin >> str;
+	str1 = str;
+	int len = str.size();
+	int n = 2;
+	while (n)
+	{	
+		if (len%8!=0)
+		{
+			for (size_t i = len; i < 8*(len/8+1); i++)
+			{
+				str1.push_back(0);
+			}
+		}
+		for (size_t i = 1; i <= str1.size(); i++)
+		{
+			cout << str1[i-1];
+			if (i%8==0)
+			{
+				cout << endl;
+			}
+		}
+		n--;
+	}*/
+
+
+	//string str;
+	//int n = 10;
+	//while (n)
+	//{
+	//	str.push_back(0);  //根本就不会输出
+	//	n--;
+	//}
+	//cout << str<<0<<0<<"0"<<endl;
+}
---
+++ 20160310_02.cpp
@@ -0,0 +1,90 @@
+﻿//题目描述
+//
+//写出一个程序，接受一个十六进制的数值字符串，输出该数值的十进制字符串。
+//
+//输入描述 :
+//输入一个十六进制的数值字符串。
+//
+//
+//输出描述 :
+//输出该数值的十进制字符串。
+
+#include<iostream>	
+#include<string>
+using namespace std;
+
+void fun(string str)
+{
+	if (str.empty())
+	{
+		return;
+	}
+	int j=0;
+	int result=0,n=1;
+	//while (str[i++] != 'x');  //读到x的下一个字符
+	int len = str.size()-1;
+
+	while (str[len]!='x') //如果没有x，一直减下去，有错
+	{
+		if (str[len]<='Z'&&str[len]>='A')
+			j = str[len] - 'A' + 10;  //字符转化为整形
+		else
+		{
+			j = str[len] - '0';
+		}
+		result = j*n + result;
+		n =n*16;
+		len--;
+		if (len==0)
+		{
+			break;
+		}
+	}
+	cout << result << endl;
+}
+int main()
+{
+	string str;
+ // 	while (cin>>str)   //死循环居然也通过了  //说在windows下输入ctrl+Z即退，有时按两次，
+	//{
+	//	fun(str);
+	//} 
+
+	//cin >> str;
+	//while (str!=" ")   //解决死循环，貌似没有
+	//{
+	//	fun(str);
+	//	cin >> str;
+	//}
+
+
+
+	//istream& getline(istream& is, string& str, char delim);  //返回值是istream类型，不能作为while退出的判断条件
+	//istream& getline(istream& is, string& str);
+	//Return Value
+	//	The same as parameter is.
+	getline(cin,str);
+	while (str != " ")   //解决死循环,空格键退出
+	{
+		fun(str);
+		getline(cin, str);
+	}
+	return 0;
+}
+//
+//#include<iostream>
+//#include <vector>
+//#include <string>
+//using namespace std;
+//int main()
+//{
+//	vector<string> vect;
+//	string str;
+//	while (cin >> str)
+//	{
+//		if (str == "0")   //这种方式也可以退出
+//			break;
+//		vect.push_back(str);
+//	}
+//	return 0;
+//}
