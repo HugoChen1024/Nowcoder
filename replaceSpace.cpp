0bb73c008a45359736771060bf9a18e845473f97
ranjiewwen <532685734@qq.com>
2016-03-23 21:27:31 +0800

    华为机测+金典

---
+++ replaceSpace.cpp
@@ -0,0 +1,53 @@
+﻿//题目描述
+//
+//请编写一个方法，将字符串中的空格全部替换为“ % 20”。假定该字符串有足够的空间存放新增的字符，
+//并且知道字符串的真实长度(小于等于1000)，同时保证字符串由大小写的英文字母组成。
+//给定一个string iniString 为原始的串，以及串的长度 int len, 返回替换后的string。
+//测试样例：
+//"Mr John Smith”,13
+//返回："Mr%20John%20Smith"
+//”Hello  World”, 12
+//返回：”Hello % 20 % 20World”
+
+#include<iostream>
+#include<string>
+#include<sstream>
+using namespace std;
+
+class Replacement {
+public:
+	string replaceSpace(string iniString, int length) {
+		// write code here
+		string result="";
+		for (size_t i = 0; i < length; i++)
+		{
+			if (iniString[i] == ' ')
+			{
+				result += "%20";
+			}
+			else
+				result += iniString[i];
+		}
+
+		//int i;
+		//while (iniString[i]!='\0')    //运行超时
+		//{
+		//	if (iniString[i]==' ')
+		//	{
+		//		iniString.insert(i,"%20");
+		//		i += 2;
+		//	}
+		//}
+
+
+		//istringstream ss(iniString);
+		//string temp,result="";
+		//while (ss>>temp)
+		//{
+		//	result += temp + "%20";  //去掉最后几个  //连续两个空格不能通过。
+		//}
+		//result.erase(result.end()-4,result.end()-1);
+
+		return result;
+	}
+};
---
+++ stringReverse.cpp
@@ -0,0 +1,79 @@
+﻿//题目描述
+//
+//将一个英文语句以单词为单位逆序排放。例如“I am a boy”，逆序排放后为“boy a am I”
+//所有单词之间用一个空格隔开，语句中除了英文字母外，不再包含其他字符
+//
+//接口说明
+///**
+//* 反转句子
+//*
+//* @param sentence 原句子
+//* @return 反转后的句子
+//*/
+//public String reverse(String sentence);
+//输入描述:
+//将一个英文语句以单词为单位逆序排放。
+//
+//
+//输出描述 :
+//得到逆序的句子
+
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<vector>
+using namespace std;
+
+//void reverse(string sentence)
+//{
+//	if (sentence.empty())
+//	{
+//		return;
+//	}
+//	vector<string> result;
+//	string  temp;  //不要定义为vector<char>
+//	for (size_t i = 0; i < sentence.size(); i++)
+//	{
+//	//	if (sentence[i]!=' ')
+//		{
+//			temp.push_back(sentence[i]);
+//		}
+//		if (sentence[i] == ' '||i==sentence.size()-1)//
+//		{
+//			result.push_back(temp);
+//			temp.clear();
+//		}
+//	//	result.push_back(temp);
+//	}
+//	for (int i = result.size(); i >0; i--)
+//	{
+//		cout << result[i-1];
+//	}
+//}
+
+void reverse(string sentence)
+{
+	istringstream ss(sentence);  //包含头文件
+	string temp,result="";
+	while (ss>>temp)//可以给vector<string>
+	{
+		if (result=="")  //第一个
+		{
+			result = temp;
+		}
+		else
+		    result = temp + ' ' + result;
+	}
+	cout << result<<endl;
+}
+int main()
+{
+	string str;
+	while (getline(cin,str))
+	{
+  		reverse(str);
+//		cout << endl;
+	}
+//	cout << endl;
+	return 0;
+}
